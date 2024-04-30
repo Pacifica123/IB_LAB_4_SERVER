67bbfb4f6d4851dda007ec6505fb8c944166b1d8
Noir <accountforprogrammingmiandedge@gmail.com>
2024-04-30 20:47:51 +0700

    NEW : start_server

--- app/observer_pattern.py
+++ app/observer_pattern.py
@@ -20,3 +20,22 @@ class Observer:
     def update(self, message):
         # TODO : отправка сообщения клиенту
         print(f'Пользователю {self.name} отправлено сообщение: {message}')
+
+
+# class ChatServer(Subject):
+#     def __init__(self):
+#         super().__init__()
+#         self.clients = []
+#
+#     def add_client(self, client):
+#         self.clients.append(client)
+#         self.attach(client)
+#
+#     def remove_client(self, client):
+#         self.clients.remove(client)
+#         self.detach(client)
+#
+#     def broadcast_message(self, message):
+#         self.notify(message)
+
+
--- app/server.py
+++ app/server.py
@@ -1,23 +1,34 @@
 import asyncio
 import socket
 
+from app.observer_pattern import Subject, Observer
+
 HOST = '127.0.0.1'
 PORT = 65432
 
+chat_server = Subject()
 
-# TODO : перенсти в Observer
+
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
     print(f'Подключение от {addr}')
+
+    client_name = await reader.read(100)
+    client_name = client_name.decode().strip()
+    chat_client = Observer(client_name)
+
+    chat_server.make_online(chat_client)
 
     while True:
         data = await reader.read(100)
         if not data:
             break
-        message = data.decode()
-        print(f'Получено сообщение: {message} от {addr}')
-        writer.write(data)
-        await writer.drain()
+        message = data.decode().strip()
+        print(f'Получено сообщение: {message} \n\n от {addr}')
+        print('-----------------------------------------------')
+        chat_server.notify(f"{client_name}: {message}")
+    chat_server.make_offline(chat_client)
+    writer.close()
 
 
 async def start():
